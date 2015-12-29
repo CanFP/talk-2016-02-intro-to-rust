a39c3d20584a37c41ec3a93b4212e53264f5f95c
Cameron <code@cameronford.net>
2015-12-29 11:11:05 +1100

    Changed ADT example to Tree

--- code/adts/src/main.rs
+++ code/adts/src/main.rs
@@ -1,33 +1,25 @@
-enum Command {
-    Download(String, String),
-    Run(String),
-    Sleep(i32),
-    ChangeSetting(String, String),
-    DumpSettings,
-}
+#![feature(box_syntax, box_patterns)]
 
-enum Direction {
-    N(i32),
-    E(i32),
-    S(i32),
-    W(i32),
-    NE(i32, i32),
-    SE(i32, i32),
-    SW(i32, i32),
-    NW(i32, i32),
+enum Tree {
+    Empty,
+    Leaf(i32),
+    Node(Tree, Tree)
 }
 
 fn main() {
-    let walk_left = Direction::W(1);
-    let run_up    = Direction::N(2);
-    let x,y = 0;
+    let one   = Tree::Leaf(1);
+    let two   = Tree::Leaf(2);
+    let three = Tree::Leaf(3);
+    let n     = Tree::Node(two, three);
+    let t     = Tree::Node(one, n);
 
-    let movement = walk_left;
+    let x = dfs(t, 2);
+}
 
-    match movement {
-        Direction::N(n) => new_pos();
-        Command::Download(web, path) => println!("Going to download {}", web),
-        Command::Run(path)           => println!("Going to run {}", path),
-        _                            => println!("Going to do .. something else")
+fn dfs(r: Tree, x: i32) -> Tree { // Convert to option return type
+    match r {
+        Tree::Leaf(i)    => r,
+        Tree::Node(a, b) => a,
+        _                => Tree::Leaf(0),
     }
 }
