d7dccca1bcb3aaac5b7d7097ad6c010915f562ca
Cameron <code@cameronford.net>
2015-12-29 11:19:22 +1100

    Tree complete with box examples

--- code/adts/src/tree.rs
+++ code/adts/src/tree.rs
@@ -3,23 +3,23 @@
 enum Tree {
     Empty,
     Leaf(i32),
-    Node(Tree, Tree)
+    Node(Box<Tree>, Box<Tree>)
 }
 
 fn main() {
     let one   = Tree::Leaf(1);
     let two   = Tree::Leaf(2);
     let three = Tree::Leaf(3);
-    let n     = Tree::Node(two, three);
-    let t     = Tree::Node(one, n);
+    let n     = Tree::Node(Box::new(two), Box::new(three));
+    let t     = Tree::Node(Box::new(one), Box::new(n));
 
     let x = dfs(t, 2);
 }
 
 fn dfs(r: Tree, x: i32) -> Tree { // Convert to option return type
     match r {
-        Tree::Leaf(i)    => r,
-        Tree::Node(a, b) => a,
-        _                => Tree::Leaf(0),
+        Tree::Leaf(i)            => r,
+        Tree::Node(box a, box b) => a,
+        _                        => Tree::Leaf(0),
     }
 }
