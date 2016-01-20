6605018089434c820626620c90af785b9664faa2
Cameron <code@cameronford.net>
2016-01-21 07:32:02 +1100

    Rust version of bo

---
+++ code/security/buffer_overflow/rust/src/main.rs
@@ -0,0 +1,56 @@
+use std::env;
+ 
+/* first pass 
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    let hello = "hello";
+    let buffer: [u8; 10] = ['\0' as u8; 10];
+
+    strcpy(buffer, args[1]);
+
+    println!("hello: {}", hello);
+    println!("buffer: {:?}", buffer);
+}
+
+fn strcpy(dst: [u8; 10], src: String) {
+    for (i, c) in src.bytes().enumerate() {
+        dst[i] = c;
+    };
+}
+*/
+
+/* Second pass
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    let hello = "hello";
+    let buffer: [u8; 10] = ['\0' as u8; 10];
+
+    strcpy(buffer, &args[1]);
+
+    println!("hello: {}", hello);
+    println!("buffer: {:?}", buffer);
+}
+
+fn strcpy(mut dst: [u8; 10], src: &String) {
+    for (i, c) in src.bytes().enumerate() {
+        dst[i] = c;
+    };
+}
+*/
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    let hello = "hello";
+    let buffer: [u8; 10] = ['\0' as u8; 10];
+
+    strcpy(buffer, &args[1]);
+
+    println!("hello: {}", hello);
+    println!("buffer: {:?}", buffer);
+}
+
+fn strcpy(&mut dst: [u8; 10], src: &String) {
+    for (i, c) in src.bytes().enumerate() {
+        dst[i] = c;
+    };
+}
