f56b0cdb510edfa1b99bd610a684116f57d6f6e5
Cameron <code@cameronford.net>
2016-02-01 21:42:05 +1100

    Buffer overflow stage 2
    Print statements now aligned

--- code/security/buffer_overflow/rust/src/main.rs
+++ code/security/buffer_overflow/rust/src/main.rs
@@ -1,20 +1,31 @@
 use std::env;
  
-// First pass
+// Second attempt
+
+// src/main.rs:10:20: 10:27 error: cannot move out of indexed content [E0507]
+// src/main.rs:10     strcpy(buffer, args[1]);
+//                                   ^~~~~~~
+// src/main.rs:18:9: 18:12 error: cannot borrow immutable argument `dst` as mutable
+// src/main.rs:18         dst[i] = c;
+//                        ^~~
+// src/main.rs:16:11: 16:14 help: to make the argument mutable, use `mut` as shown:
+// src/main.rs:   fn strcpy(mut dst: [u8; 10], src: String) {
 
 fn main() {
     let args: Vec<_> = env::args().collect();
     let hello = "hello";
     let buffer: [u8; 10] = ['\0' as u8; 10];
 
-    strcpy(buffer, args[1]);
+    strcpy(buffer, &args[1]);
 
-    println!("hello: {}", hello);
-    println!("buffer: {:?}", buffer);
+    println!("hello:      {}", hello);
+    println!("buffer:     {:?}", buffer);
 }
 
-fn strcpy(dst: [u8; 10], src: String) {
+fn strcpy(mut dst: [u8; 10], src: &String) {
+    println!("dst before: {:?}", dst);
     for (i, c) in src.bytes().enumerate() {
         dst[i] = c;
     };
+    println!("dst after:  {:?}", dst);
 }
