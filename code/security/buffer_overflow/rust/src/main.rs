edaff215e77282fe0259fd84dae1d8f31ce2030b
Cameron <code@cameronford.net>
2016-02-16 20:38:02 +1100

    Basic ownership example running

--- code/security/buffer_overflow/rust/src/main.rs
+++ code/security/buffer_overflow/rust/src/main.rs
@@ -14,15 +14,15 @@ use std::env;
 fn main() {
     let args: Vec<_> = env::args().collect();
     let hello = "hello";
-    let buffer: [u8; 10] = ['\0' as u8; 10];
+    let mut buffer = ['\0' as u8; 10];
 
-    strcpy(buffer, &args[1]);
+    strcpy(&buffer, &args[1]);
 
     println!("hello:      {}", hello);
     println!("buffer:     {:?}", buffer);
 }
 
-fn strcpy(mut dst: [u8; 10], src: &String) {
+fn strcpy(dst: &mut [u8; 10], src: &String) {
     println!("dst before: {:?}", dst);
     for (i, c) in src.bytes().enumerate() {
         dst[i] = c;
