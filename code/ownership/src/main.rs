7c52f59b7affcde65843653162798b8fde2c6c67
Cameron <code@cameronford.net>
2016-02-16 20:52:01 +1100

    Move success with borrowing

--- code/ownership/src/main.rs
+++ code/ownership/src/main.rs
@@ -1,9 +1,8 @@
 fn main() {
-    let mut v1 = vec![1,2,3,4];
-    v1 = foo(v1);
+    let v1 = vec![1,2,3,4];
+    foo(&v1);
     println!("{:?}", v1);
 }
 
-fn foo(v: Vec<i32>) -> Vec<i32> {
-    return v
+fn foo(v: &Vec<i32>){
 }
