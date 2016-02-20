a606bc2dd207367eb7688aa0a474c0542210d304
Cameron <code@cameronford.net>
2016-02-20 20:32:19 +1100

    ADTs progress
    Inital commit
    
    Hello world project added
    
    build dirs now ignored
    
    Add immmutability example
    
    Start ADT example
    
    Update immutable example to modify rather than re-assign
    
    Re-jigging the ADT example
    
    Changed ADT example to Tree

--- code/adts/src/main.rs
+++ code/adts/src/main.rs
@@ -1,5 +1,5 @@
 #![feature(box_syntax, box_patterns)]
-//https://doc.rust-lang.org/book/box-syntax-and-patterns.html
+// https://doc.rust-lang.org/book/box-syntax-and-patterns.html
 
 enum Tree {
     Empty,
@@ -11,19 +11,6 @@ fn main() {
     let one   = Tree::Leaf(1);
     let two   = Tree::Leaf(2);
     let three = Tree::Leaf(3);
-    //let n     = Tree::Node(Box::new(two), Box::new(three));
     let n     = Tree::Node(box two, box three);
     let t     = Tree::Node(box one, box n);
-
-    //dfs(t, 2);
 }
-/*
-fn dfs(Tree: t, i32: n) -> Tree {
-    match movement {
-        Direction::N(n) => new_pos();
-        Command::Download(web, path) => println!("Going to download {}", web),
-        Command::Run(path)           => println!("Going to run {}", path),
-        _                            => println!("Going to do .. something else")
-    }
-}
-*/
